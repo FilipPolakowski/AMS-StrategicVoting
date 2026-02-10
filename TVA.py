f2bb57b28d39f60fb3973675e61b4dfc8667e6e8
FilipPolakowski <91736314+FilipPolakowski@users.noreply.github.com>
2026-02-10 15:57:44 +0100

    Some happines + plurality
    SDASA

--- TVA.py
+++ TVA.py
@@ -78,6 +78,41 @@ def print_voting_situation(voting_situation, voters, preferences):
             print(f" {voting_situation[r][v]} ", end="")
         print()
 
+
+def plurality_voting(voting_situation, candidates, voters, preferences):
+    scores = {c: 0 for c in candidates}
+
+    for v in range(voters):
+        for r in range(preferences):
+            if r == 0:  # only first rank counts because plurality
+                candidate = voting_situation[r][v]
+                scores[candidate] += 1
+
+    max_score = max(scores.values())
+    winners = [c for c, s in scores.items() if s == max_score]
+
+    return scores, winners
+
+def compute_happiness(voting_situation, winner, voters, preferences):
+    happiness_per_voter = []
+    total_happiness = 0
+
+    for v in range(voters):
+        voter_happiness = 0
+
+        for r in range(preferences):
+            if voting_situation[r][v] == winner:
+                voter_happiness = 1 / (r + 1)
+                break
+
+        happiness_per_voter.append(voter_happiness)
+        total_happiness += voter_happiness
+
+    average_happiness = total_happiness / voters
+
+    return happiness_per_voter, average_happiness
+
+
 if __name__ == '__main__':
     voting_situation, candidates, voters, preferences = get_voting_situation()
     
@@ -87,3 +122,10 @@ if __name__ == '__main__':
     print(voters)
     print(preferences)
     print_voting_situation(voting_situation,voters, preferences)
+    scores,winners = plurality_voting(voting_situation,candidates,voters,preferences)
+    print(scores)
+    print(winners)
+    winner = winners[0]
+    happiness_per_voter, average_happiness = compute_happiness(voting_situation,winner,voters, preferences)
+    print(happiness_per_voter)
+    print(average_happiness)
